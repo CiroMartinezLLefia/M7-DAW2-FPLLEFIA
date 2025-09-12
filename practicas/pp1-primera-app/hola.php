b702228edc588fe477a8d0a212ef2c8c71eec264
CiroMartinezLLefia <martinezmartinciro@fpllefia.com>
2025-09-12 15:58:39 +0000

    first

--- practicas/pp1-primera-app/hola.php
+++ practicas/pp1-primera-app/hola.php
@@ -1,21 +1,35 @@
 <?php
 
 function sayHello($name) {
-  echo "Hello $name !";
+  echo "Hello $name!";
 }
 ?>
 
-<html>
+<!DOCTYPE html>
+<html lang="es">
   <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Visual Studio Remote :: PHP</title>
   </head>
+
   <body>
-    <?php
+    <header>
+      <img src="img/logo-fpllefia.jfif" alt="logo">
+      <h1>Módulo 7 - Práctica 1. Mi primera aplicación en PHP</h1>
+    </header>
 
-      sayHello('remote world');
+    <main>
+      <?php
+        sayHello('remote world');
+      ?>
+      <img src="img/yo.jpg" alt="yo">
+      <p>Texto</p>
+    </main>
 
-      phpinfo();
-
-    ?>
+    <footer>
+      <p>Ciro Martinez</p>
+      <p>2025-12-09</p>
+    </footer>
   </body>
 </html>
