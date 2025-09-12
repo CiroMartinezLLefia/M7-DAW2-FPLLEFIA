ebbfa006357b3cb73bb36b71cb57613d51990c05
CiroMartinezLLefia <martinezmartinciro@fpllefia.com>
2025-09-12 17:11:20 +0200

    Create hola.php

---
+++ practicas/pp1-primera-app/hola.php
@@ -0,0 +1,21 @@
+<?php
+
+function sayHello($name) {
+  echo "Hello $name !";
+}
+?>
+
+<html>
+  <head>
+    <title>Visual Studio Remote :: PHP</title>
+  </head>
+  <body>
+    <?php
+
+      sayHello('remote world');
+
+      phpinfo();
+
+    ?>
+  </body>
+</html>
