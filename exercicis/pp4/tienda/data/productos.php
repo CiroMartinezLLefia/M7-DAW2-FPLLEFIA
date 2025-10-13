734edc9d05bcc90eb7d7a11d286bff00fecc68dd
CiroMartinezLLefia <martinezmartinciro@fpllefia.com>
2025-10-13 13:53:13 +0000

    Creacion de la lista/array

---
+++ exercicis/pp4/tienda/data/productos.php
@@ -0,0 +1,29 @@
+<?php
+    $productos = [
+        [
+            'nombre' => "platano", 
+            'precio' => 12.23, 
+            'disponibilidad' => false
+        ],
+        [
+            'nombre' => "platano1", 
+            'precio' => 12.25, 
+            'disponibilidad' => true            
+        ],
+        [
+            'nombre' => "cereal", 
+            'precio' => 2.03, 
+            'disponibilidad' => true            
+        ],
+        [
+            'nombre' => "donus", 
+            'precio' => 0.56, 
+            'disponibilidad' => false            
+        ],
+        [
+            'nombre' => "macarrones", 
+            'precio' => 15.8, 
+            'disponibilidad' => true            
+        ],
+    ]
+?>
