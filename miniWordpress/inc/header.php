7bd75221c38284b964f62761aa665cabff895f5f
CiroMartinezLLefia <martinezmartinciro@fpllefia.com>
2025-10-20 15:43:11 +0000

    login v0.2

--- miniWordpress/inc/header.php
+++ miniWordpress/inc/header.php
@@ -2,8 +2,17 @@
     <h1>:) Mi WordPress con array multidimensional</h1>
     <nav>
         <ul>
-            <li><a href="form.php">Añadir noticia</a></li>
-            <li><a href="index.php?clear=1">REINICIAR NOTICAS</a></li>
+            <?php
+                if (!true)
+                {
+                    echo '<li><a href="login.php">Login</a></li>';
+                }
+                else
+                {
+                    echo '<li><a href="form.php">Añadir noticia</a></li>';
+                    echo '<li><a href="index.php?clear=1">Reiniciar Noticias</a></li>';
+                }
+            ?>
         </ul>
     </nav>
 </header>
